9124ad3a687e2083a9b4535b9ac514fda0bc4665
JuanSeSaCa <156488290+JuanSeSaCa@users.noreply.github.com>
2024-06-04 10:46:14 -0500

    feat: :sparkles: 3. orco, goblin, kobold

--- monstruo.js
+++ monstruo.js
@@ -8,3 +8,21 @@
     }
   }
   
+  class Orco extends Monstruo {
+    constructor() {
+      super('Orco', 100, 100, 20);
+    }
+  }
+  
+  class Goblin extends Monstruo {
+    constructor() {
+      super('Goblin', 50, 50, 15);
+    }
+  }
+  
+  class Kobold extends Monstruo {
+    constructor() {
+      super('Kobold', 30, 30, 10);
+    }
+  }
+  
